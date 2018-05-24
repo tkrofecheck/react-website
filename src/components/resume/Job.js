11a619e484a44e15cf0c8428e067ea34d5b8e2a6
Tim Krofecheck <tkrofecheck@gmail.com>
2018-05-23 22:46:03 -0400

    remove 'Component' from ES6 react package import

--- src/components/resume/Job.js
+++ src/components/resume/Job.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Company from './Company';
 import City from './City';
 import Role from './Role';
--- src/components/resume/Skill.js
+++ src/components/resume/Skill.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const Skill = (props) => {
     return (
