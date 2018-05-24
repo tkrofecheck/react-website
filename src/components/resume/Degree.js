649123875cbaa4848d80fc98c2657e6404d83b61
Tim Krofecheck <tkrofecheck@gmail.com>
2018-05-23 21:54:16 -0400

    add education section with degrees

---
+++ src/components/resume/Degree.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+const Degree = (props) => {
+    return (
+        <div className="degree">
+            <div className="degree-type">
+                {props.type}
+            </div>
+            <div className="degree-focus">
+                {props.focus}
+            </div>
+            <div className="degree-year">
+                {props.year}
+            </div>
+        </div>
+    );
+}
+
+export default Degree;
--- src/components/resume/Section.js
+++ src/components/resume/Section.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Job from './Job';
+import Degree from './Degree';
 
 export default class Section extends Component {
     constructor(props) {
@@ -8,7 +9,7 @@ export default class Section extends Component {
         this.getJobs = this.getJobs.bind(this);
     }
     
-    getJobs(data) {
+    getJobs() {
         const { jobs } = this.props;
 
         if (jobs !== undefined) {
@@ -17,6 +18,32 @@ export default class Section extends Component {
                     {jobs.map((job, index) => {
                         return <Job key={index} {...job} />;
                     })}
+                </div>
+            )
+        }
+    }
+
+    getEducation() {
+        const { college } = this.props;
+
+        if (college !== undefined) {
+            const { name, city, degrees } = college;
+
+            return(
+                <div className="resume_education">
+                    <div className="resume_education-school">
+                        <div className="name">
+                            {name}
+                        </div>
+                        <div className="city">
+                            {city}
+                        </div>
+                        <div className="degree">
+                            {degrees.map((degree, index) => {
+                                return <Degree key={index} {...degree} />;
+                            })}
+                        </div>
+                    </div>
                 </div>
             )
         }
@@ -31,6 +58,7 @@ export default class Section extends Component {
                 {name}
                 {text}
                 {this.getJobs()}
+                {this.getEducation()}
             </section>
         )
     }
