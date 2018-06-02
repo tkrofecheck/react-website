7c256a13e9c400cb553420cc39924dc399f42447
Tim Krofecheck <tkrofecheck@gmail.com>
2018-06-01 21:46:03 -0400

    bio component update using data for all elements

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import Data from '../data/data.json';
 
-const Bio = () => (
-    <div className="bio">
-        <div className="container">
-            <div className="bio__header">
-                <h1 className="bio__header-name">Tim Krofecheck</h1>
-                <div className="bio__header-title">Frontend Web Developer</div>
+const Bio = () => {
+    const { name, title, text } = Data.bio;
+    
+    return (
+        <div className="bio">
+            <div className="container">
+                <div className="bio__header">
+                    <h1 className="bio__header-name">{name}</h1>
+                    <div className="bio__header-title">{title}</div>
+                </div>
+                <div className="bio-text">{text}</div>
             </div>
-            <div className="bio-text">{Data.bio}</div>
         </div>
-    </div>
-);
+    );
+};
 
 export default Bio;
